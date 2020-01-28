b76dabf0b42d09b2353f079626088acdff1574ac
Jared Stokes <jared_vegeta@yahoo.com>
2020-01-27 21:27:02 -0600

    Initial u-ball unity project commit

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public GameObject Player;
+    public float CameraRotationSmoothingCoefficient; // A bigger number makes the camera more jittery but more responsive.
+    public float CameraPullawayCoefficient;
+
+    private Rigidbody _playerRb;
+    private Vector3 _offset;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _playerRb = Player.GetComponent<Rigidbody>();
+        _offset = transform.position - Player.transform.position;
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        var lookRotation = Quaternion.LookRotation(Player.transform.position - transform.position);
+        transform.rotation = Quaternion.Lerp(transform.rotation, lookRotation, Time.deltaTime * CameraRotationSmoothingCoefficient);
+        //transform.LookAt(Player.transform.position, Vector3.left);
+        
+        if (_playerRb.velocity.magnitude != 0)
+        {
+            var playerVelocity = _playerRb.velocity;
+
+            // Since you currently can't move in the y plane, set the y component to the negative of the 
+            // the initial y offset so that the camera will remain slightly above the ball.
+            playerVelocity.y = (-1) * _offset.y;
+            transform.position = Player.transform.position - Vector3.Scale(playerVelocity, new Vector3(CameraPullawayCoefficient, CameraPullawayCoefficient, CameraPullawayCoefficient));
+        }
+    }
+
+    //Vector3 GetBehindPosition(Transform target, float distanceBehind, float distanceAbove)
+    //{
+    //    return target.position - (target.forward * distanceBehind) + (target.up * distanceAbove*0);
+    //}
+
+    //float facing = Camera.main.transform.eulerAngles.y
+
+    //Vector3 myInputs = ... (however you get X and Y into here);
+    //// we rotate them around Z, assuming your inputs are in X and Y in the myInputs vector
+    //Vector3 myTurnedInputs = Quaternion.Euler(0, 0, facing) * myInput;
+
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour
+{
+    [Range(10,20)]
+    public float Speed;
+    public float JumpVelocity;
+    public float FallMultiplier = 2.5f;
+    public float LowJumpMultiplier = 2f;
+
+    public Text CountText;
+    public Text WinText;
+
+    public GameObject Camera;
+    
+    private Rigidbody _rb;
+    private int _count = 0;
+
+    void Start()
+    {
+        _rb = GetComponent<Rigidbody>();
+        _count = 0;
+        SetCountText();
+        WinText.text = string.Empty;
+    }
+
+    // TODO: Shift to boost.
+    // TODO: Billards where you play as the cue ball.
+    void Update()
+    {        
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+        Vector3 input = new Vector3(moveHorizontal, 0, moveVertical);
+
+        if (Input.GetKeyDown(KeyCode.Space) && _rb.velocity.y == 0)
+        {
+            input.y = JumpVelocity;
+        }
+
+        if (_rb.velocity.y < 0)
+        {
+            input += Vector3.up * Physics.gravity.y * (FallMultiplier - 1) * Time.deltaTime;
+        }
+        else if (_rb.velocity.y > 0 && !Input.GetKey(KeyCode.Space))
+        {
+            input += Vector3.up * Physics.gravity.y * (LowJumpMultiplier - 1) * Time.deltaTime;
+        }
+
+        float facing = Camera.transform.eulerAngles.y;
+
+        //Vector3 myInputs = ... (however you get X and Y into here);
+        //// we rotate them around Y, assuming your inputs are in X and Z in the myInputs vector
+        Vector3 myTurnedInputs = Quaternion.Euler(0, facing, 0) * input;
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+        _rb.AddForce(myTurnedInputs * Speed);       
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Pick Up"))
+        {
+            other.gameObject.SetActive(false);
+            _count++;
+            SetCountText();
+        }
+    }
+
+    private void SetCountText()
+    {
+        CountText.text = "Count: " + _count.ToString();
+
+        if (_count >= 10)
+        {
+            WinText.text = "You're Winner!";
+        }
+    }
+}
